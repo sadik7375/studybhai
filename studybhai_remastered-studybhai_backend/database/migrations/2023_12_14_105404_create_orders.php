4732d47c2871a88ce6bb5dac5004841979cca849
sadik7375 <wahidsadik7375@gmail.com>
2023-12-14 18:03:41 +0600

    all code

---
+++ studybhai_remastered-studybhai_backend/database/migrations/2023_12_14_105404_create_orders.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->decimal('amount', 10, 2);
+            $table->string('status');
+            $table->text('address');
+            $table->string('transaction_id');
+            $table->string('currency');
+            // Add more columns as needed for your specific use case
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
